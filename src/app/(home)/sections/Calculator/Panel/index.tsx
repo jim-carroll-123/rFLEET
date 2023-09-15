655583f3f6fbf7e27ae844775adbf4ed7f9c5816
Jason Mendoza <mernstackdev729@gmail.com>
2023-09-15 17:50:38 -0400

    chore: added 'origin' tab pane

--- src/app/(home)/sections/Calculator/Panel/index.tsx
+++ src/app/(home)/sections/Calculator/Panel/index.tsx
@@ -2,15 +2,13 @@
 
 import { useState } from 'react'
 
-import ArrowRight from '@assets/icons/arrow-right.svg'
-import User from '@assets/icons/user.svg'
 import { Button } from '@components/ui/Button'
 import { ButtonSelect } from '@components/ui/ButtonSelect'
 import { Check } from '@components/ui/Check'
-import { Input } from '@components/ui/Input'
-import { Select } from '@components/ui/Select'
 import { Pane, Tab, TabPane } from '@components/ui/TabPane'
 import { cn } from '@lib/utils'
+
+import { From } from './Panes/From'
 
 const types = [
   { label: 'Parcel', value: 'Parcel' },
@@ -20,32 +18,6 @@ const types = [
   {
     label: 'FTL, Power Only, Drayage,\nor Oversize & Overweight',
     value: 'FTL, Power Only, Drayage, or Oversize & Overweight'
-  }
-]
-
-const fromTypes = [
-  {
-    label: 'Port/Airport',
-    value: 'Port/Airport'
-  },
-  {
-    label: 'Factory/Wearhouse',
-    value: 'Factory/Wearhouse'
-  },
-  {
-    label: 'Business Address',
-    value: 'Business Address'
-  },
-  {
-    label: 'Residential Address',
-    value: 'Residential Address'
-  }
-]
-
-const countries = [
-  {
-    label: 'Abkhazia',
-    value: 'Abkhazia'
   }
 ]
 
@@ -71,7 +43,7 @@ export const Panel = () => {
             <Check label="Add Extra Drops" />
           </div>
           <div>
-            <CategoryTab target="3" label="Load Details" className="mb-6" category={category}>
+            <CategoryTab target="3" label="Load Type" className="mb-6" category={category}>
               What are you shipping?
             </CategoryTab>
           </div>
@@ -84,37 +56,7 @@ export const Panel = () => {
         </div>
         <div>
           <CategoryPane id="1">
-            <h4 className="font-semibold">Where are you shipping from?</h4>
-            <div className="flex lg:flex-row flex-col flex-1 lg:gap-[24px] gap-[18px]">
-              <div className="flex-1 lg:grid lg:grid-cols-3 flex flex-col lg:gap-[16px] gap-[12px]">
-                <div>
-                  <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Type</div>
-                  <Select options={fromTypes} placeholder="Tell us about your goods" />
-                </div>
-                <div>
-                  <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Country</div>
-                  <Select options={countries} placeholder="Select a country" />
-                </div>
-                <div>
-                  <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Address</div>
-                  <Input placeholder="Enter Address or Zip/Postal Code" />
-                </div>
-              </div>
-              <div className="flex items-end">
-                <Button className="lg:!px-[24px] !px-[18px]">
-                  <ArrowRight />
-                </Button>
-              </div>
-            </div>
-            <div className="w-full h-[2px] opacity-70 bg-gradient-to-r-from-green-to-violet" />
-            <div className="flex flex-col lg:gap-[4px] gap-[3px]">
-              <div className="text-caption-sm">My recent searches</div>
-              <div className="flex items-center lg:gap-[8px] gap-[6px]">
-                <User />
-                <h6>Login / Sign Up</h6>
-              </div>
-              <div className="text-caption-sm">Access your searches on any devices</div>
-            </div>
+            <From />
           </CategoryPane>
           <Pane id="2" className="absolute">
             Pane 2
