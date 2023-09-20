e8900af9e0ad3e55bc0ed4ba49c1c569b85d31a0
Jason Mendoza <mernstackdev729@gmail.com>
2023-09-20 10:47:52 -0400

    chore: updated radio component

--- src/components/ui/Radio.tsx
+++ src/components/ui/Radio.tsx
@@ -5,11 +5,12 @@ import { cn } from '@lib/utils'
 interface Props {
   label?: string
   checked?: boolean
+  value?: string
   placeholder?: string
   searchable?: boolean
   containerClassName?: string
   labelClassName?: string
-  onChange?: (newValue: boolean) => void | Promise<void>
+  onChange?: (v: string) => void | Promise<void>
 }
 
 export const Radio = (props: Props) => {
@@ -30,6 +31,9 @@ export const Radio = (props: Props) => {
           id={id}
           name="type"
           type="radio"
+          checked={props.checked}
+          value={props.value}
+          onChange={(e) => (props.onChange ? props.onChange(e.target.value) : false)}
           className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border-2 border-blue-gray-200 text-primary transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-primary checked:before:bg-primary hover:before:opacity-10"
         />
         <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-primary opacity-0 transition-opacity peer-checked:opacity-100">
