f5240235052c5f2f737186fb4ed2069b607c5689
Jason Mendoza <mernstackdev729@gmail.com>
2023-09-20 10:09:55 -0400

    chore: added hook form in To, From form

--- src/app/(home)/sections/Calculator/Panel/Panes/From.tsx
+++ src/app/(home)/sections/Calculator/Panel/Panes/From.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { SubmitHandler, UseFormReturn } from 'react-hook-form'
 
 import ArrowRight from '@assets/icons/arrow-right.svg'
 import BusinessAddress from '@assets/icons/business-address.svg'
@@ -11,7 +11,9 @@ import User from '@assets/icons/user.svg'
 import { Button } from '@components/ui/Button'
 import { CountrySelect } from '@components/ui/CountrySelect'
 import { Input } from '@components/ui/Input'
-import { Option, Select } from '@components/ui/Select'
+import { Select } from '@components/ui/Select'
+
+import { FromInputs } from '..'
 
 const fromTypes = [
   {
@@ -37,42 +39,56 @@ const fromTypes = [
 ]
 
 interface Props {
-  next: () => void
+  methods: UseFormReturn<FromInputs>
+  onSubmit: SubmitHandler<FromInputs>
 }
 
-export const From = ({ next }: Props) => {
-  const [fromType, setFromType] = useState<Option>()
-  const [fromCountry, setFromCountry] = useState<Option>()
+export const From = ({ methods, onSubmit }: Props) => {
+  const {
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { errors }
+  } = methods
 
   return (
     <>
       <h4 className="font-semibold">Where are you shipping from?</h4>
-      <div className="flex lg:flex-row flex-col flex-1 gap-d-24">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex lg:flex-row flex-col flex-1 gap-d-24">
         <div className="flex-1 lg:grid lg:grid-cols-3 flex flex-col gap-d-16">
           <div>
             <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Type</div>
             <Select
               options={fromTypes}
-              value={fromType}
+              value={watch('fromType')}
+              onChange={(value) => setValue('fromType', value)}
               placeholder="Tell us about your goods"
-              onChange={setFromType}
             />
           </div>
           <div>
             <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Country</div>
-            <CountrySelect searchable value={fromCountry} placeholder="Select a country" onChange={setFromCountry} />
+            <CountrySelect
+              searchable
+              value={watch('fromCountry')}
+              placeholder="Select a country"
+              onChange={(value) => setValue('fromCountry', value)}
+            />
           </div>
           <div>
             <div className="font-semibold text-gray-200 lg:mb-[12px] mb-[8px]">Address</div>
-            <Input placeholder="Enter Address or Zip/Postal Code" />
+            <Input
+              value={watch('fromAddress')}
+              onChange={(value) => setValue('fromAddress', value)}
+              placeholder="Enter Address or Zip/Postal Code"
+            />
           </div>
         </div>
         <div className="flex items-end">
-          <Button className="lg:!px-[24px] !px-[18px]" onClick={next}>
+          <Button type="submit" className="lg:!px-[24px] !px-[18px]">
             <ArrowRight />
           </Button>
         </div>
-      </div>
+      </form>
       <div className="w-full h-[2px] opacity-70 bg-gradient-to-r-from-green-to-violet" />
       <div className="flex flex-col lg:gap-[4px] gap-[3px]">
         <div className="text-caption-sm">My recent searches</div>
--- src/app/(home)/sections/Calculator/Panel/index.tsx
+++ src/app/(home)/sections/Calculator/Panel/index.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Button } from '@components/ui/Button'
 import { ButtonSelect } from '@components/ui/ButtonSelect'
 import { Check } from '@components/ui/Check'
+import { Option } from '@components/ui/Select'
 import { TabPane } from '@components/ui/TabPane'
 
 import { From } from './Panes/From'
@@ -14,24 +16,56 @@ import { To } from './Panes/To'
 import { ShippingPane } from './ShippingPane'
 import { ShippingStep } from './ShippingStep'
 
-const types = [
-  { label: 'Parcel', value: 'Parcel' },
-  { label: 'LTL & Partials', value: 'LTL & Partials' },
-  { label: 'Ocean Shipping', value: 'Ocean Shipping' },
-  { label: 'Air Cargo', value: 'Air Cargo' },
-  {
-    label: 'FTL, Power Only, Drayage,\nor Oversize & Overweight',
-    value: 'FTL, Power Only, Drayage, or Oversize & Overweight'
-  }
-]
+export type FromInputs = {
+  fromType: Option
+  fromCountry: Option
+  fromAddress: string
+}
+
+export type ToInputs = {
+  toType: Option
+  toCountry: Option
+  toAddress: string
+}
 
 export const Panel = () => {
-  const [type, setType] = useState(types[0])
+  const shippingMethods = [
+    { label: 'Parcel', value: 'Parcel' },
+    { label: 'LTL & Partials', value: 'LTL & Partials' },
+    { label: 'Ocean Shipping', value: 'Ocean Shipping' },
+    { label: 'Air Cargo', value: 'Air Cargo' },
+    {
+      label: 'FTL, Power Only, Drayage,\nor Oversize & Overweight',
+      value: 'FTL, Power Only, Drayage, or Oversize & Overweight'
+    }
+  ]
+
+  const [shippingMethod, setShippingMethod] = useState(shippingMethods[0])
   const [shippingStepId, setShippingStepId] = useState('')
+
+  const fromFormMethods = useForm<FromInputs>({
+    defaultValues: {
+      fromAddress: ''
+    }
+  })
+
+  const toFormMethods = useForm<ToInputs>({
+    defaultValues: {
+      toAddress: ''
+    }
+  })
+
+  const onFromFormSubmit: SubmitHandler<FromInputs> = (data) => console.debug(data)
+  const onToFormSubmit: SubmitHandler<ToInputs> = (data) => console.debug(data)
 
   return (
     <div className="relative bg-gradient-blur-dialog border border-solid border-[#ffffff30] p-[26px] lg:p-[36px] rounded-[20px]">
-      <ButtonSelect options={types} value={type} onChange={setType} containerClassName="mb-[28px]" />
+      <ButtonSelect
+        options={shippingMethods}
+        value={shippingMethod}
+        onChange={setShippingMethod}
+        containerClassName="mb-[28px]"
+      />
       <TabPane className="relative" activeTab={shippingStepId} onTabChange={(id) => setShippingStepId(id)}>
         <div className="lg:grid lg:grid-cols-4 lg:gap-[16px] flex flex-col gap-[30px] lg:mb-[34px] mb-[26px]">
           <div>
@@ -75,10 +109,10 @@ export const Panel = () => {
         </div>
         <div>
           <ShippingPane id="tab-ship-origin">
-            <From next={() => setShippingStepId('tab-ship-destination')} />
+            <From methods={fromFormMethods} onSubmit={onFromFormSubmit} />
           </ShippingPane>
           <ShippingPane id="tab-ship-destination">
-            <To next={() => setShippingStepId('tab-ship-load-type')} />
+            <To methods={toFormMethods} onSubmit={onToFormSubmit} />
           </ShippingPane>
           <ShippingPane id="tab-ship-load-type">
             <LoadType next={() => setShippingStepId('tab-ship-goods-commodity')} />
