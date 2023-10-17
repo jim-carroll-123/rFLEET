d606dd28d9dac7fdf2eb6fc849b039fdd951806a
Luke Cutting <lukecutting123@gmail.com>
2023-10-17 17:32:14 -0500

    configuring .env files

--- src/app/(home)/sections/Search/Botframework.tsx
+++ src/app/(home)/sections/Search/Botframework.tsx
@@ -3,11 +3,11 @@ import React, { useMemo } from 'react'
 import ReactWebChat, { createDirectLine } from 'botframework-webchat'
 
 const BotFrameworkChat = () => {
-  const BOT_FRAMEWORK_TOKEN = process.env.REACT_APP_BOT_FRAMEWORK_TOKEN
+  const BOT_FRAMEWORK_TOKEN = process.env.NEXT_PUBLIC_BOT_FRAMEWORK_TOKEN
 
   const directLine = useMemo(() => createDirectLine({ token: BOT_FRAMEWORK_TOKEN }), [])
-
-  return <ReactWebChat directLine={directLine} userID="YOUR_USER_ID" />
+  const userID = useMemo(() => `user-${Math.random().toString(36).substr(2, 9)}`, [])
+  return <ReactWebChat directLine={directLine} userID={userID} />
 }
 
 export default BotFrameworkChat
