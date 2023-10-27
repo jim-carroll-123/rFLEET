1250181cc30c651cfb51661792b1de4e190a25d9
Luke Cutting <lukecutting123@gmail.com>
2023-10-27 11:57:00 -0500

    setting up Display Rate

--- src/app/(home)/sections/Calculator/Panel/ShippingSteps.tsx
+++ src/app/(home)/sections/Calculator/Panel/ShippingSteps.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Button } from '@components/ui/Button';
-import { Check } from '@components/ui/Check';
+import { useState } from 'react'
+
+import { Button } from '@components/ui/Button'
+import { Check } from '@components/ui/Check'
+import { GradientHR } from '@components/ui/GradientHR'
 import { Tab } from '@components/ui/TabPane';
 import countries from '@json/countries.json';
 import { cn } from '@lib/utils';
-
 
 interface ShippingStepsProps {
   shippingStepId: string
@@ -72,92 +74,111 @@ const handleButtonClick = (e: { preventDefault: () => void }, data: any) => {
 }
 
 export const ShippingSteps = ({ shippingStepId, data }: ShippingStepsProps) => {
+  const [isDisplayRate, setDisplayRate] = useState(false)
   return (
-    <div className="flex lg:flex-row flex-col gap-d-16">
-      <div className="grow">
-        <div className="lg:grid lg:grid-cols-4 gap-d-12 flex flex-col lg:mb-[8px] mb-[6px]">
-          <ShippingStep
-            target="tab-ship-origin"
-            label="Origin"
-            className="overflow-x-hidden"
-            shippingStepId={shippingStepId}
-          >
-            {data.fromType ? (
-              <div className="flex items-center gap-d-10 text-white font-bold">
-                <div className="flex items-center shrink-0 lg:gap-[4px] gap-[3px]">
-                  {
-                    <img
-                      src={countries.find((country) => country.code === data.fromCountry)?.flag}
-                      className="w-[24px] h-[18px]"
-                    />
-                  }
+    <>
+      <div className="flex lg:flex-row flex-col gap-d-16">
+        <div className="grow">
+          <div className="lg:grid lg:grid-cols-4 gap-d-12 flex flex-col lg:mb-[8px] mb-[6px]">
+            <ShippingStep
+              target="tab-ship-origin"
+              label="Origin"
+              className="overflow-x-hidden"
+              shippingStepId={shippingStepId}
+            >
+              {data.fromType ? (
+                <div className="flex items-center gap-d-10 text-white font-bold">
+                  <div className="flex items-center shrink-0 lg:gap-[4px] gap-[3px]">
+                    {
+                      <img
+                        src={countries.find((country) => country.code === data.fromCountry)?.flag}
+                        className="w-[24px] h-[18px]"
+                      />
+                    }
+                  </div>
+                  <div className="text-input">{data.fromType}</div>
                 </div>
-                <div className="text-input">{data.fromType}</div>
-              </div>
-            ) : (
-              'Where are you shipping from?'
-            )}
-          </ShippingStep>
-          <ShippingStep
-            target="tab-ship-destination"
-            label="Destination"
-            className="overflow-x-hidden"
-            shippingStepId={shippingStepId}
-          >
-            {data.toType ? (
-              <div className="flex items-center gap-d-10 text-white font-bold">
-                <div className="flex items-center shrink-0 lg:gap-[4px] gap-[3px]">
-                  {
-                    <img
-                      src={countries.find((country) => country.code === data.toCountry)?.flag}
-                      className="w-[24px] h-[18px]"
-                    />
-                  }
+              ) : (
+                'Where are you shipping from?'
+              )}
+            </ShippingStep>
+            <ShippingStep
+              target="tab-ship-destination"
+              label="Destination"
+              className="overflow-x-hidden"
+              shippingStepId={shippingStepId}
+            >
+              {data.toType ? (
+                <div className="flex items-center gap-d-10 text-white font-bold">
+                  <div className="flex items-center shrink-0 lg:gap-[4px] gap-[3px]">
+                    {
+                      <img
+                        src={countries.find((country) => country.code === data.toCountry)?.flag}
+                        className="w-[24px] h-[18px]"
+                      />
+                    }
+                  </div>
+                  <div className="text-input">{data.toType}</div>
                 </div>
-                <div className="text-input">{data.toType}</div>
-              </div>
-            ) : (
-              'Where are you shipping to?'
-            )}
-          </ShippingStep>
-          <ShippingStep
-            target="tab-ship-load-type"
-            label="Load Type"
-            className="overflow-x-hidden"
-            shippingStepId={shippingStepId}
-          >
-            {data.parcelType ? (
-              <div className="flex items-center gap-d-10 text-white font-bold">
-                <div className="text-input">
-                  {data.parcelType === 'Enter Custom Dimensions' ? 'Custom Dimensions' : data.parcelType}
+              ) : (
+                'Where are you shipping to?'
+              )}
+            </ShippingStep>
+            <ShippingStep
+              target="tab-ship-load-type"
+              label="Load Type"
+              className="overflow-x-hidden"
+              shippingStepId={shippingStepId}
+            >
+              {data.parcelType ? (
+                <div className="flex items-center gap-d-10 text-white font-bold">
+                  <div className="text-input">
+                    {data.parcelType === 'Enter Custom Dimensions' ? 'Custom Dimensions' : data.parcelType}
+                  </div>
                 </div>
-              </div>
-            ) : (
-              'What are you shipping?'
-            )}
-          </ShippingStep>
-          <ShippingStep
-            target="tab-ship-goods-commodity"
-            label="Goods/Commodity"
-            className="overflow-x-hidden"
-            shippingStepId={shippingStepId}
-          >
-            Goods/Commodity
-          </ShippingStep>
+              ) : (
+                'What are you shipping?'
+              )}
+            </ShippingStep>
+            <ShippingStep
+              target="tab-ship-goods-commodity"
+              label="Goods/Commodity"
+              className="overflow-x-hidden"
+              shippingStepId={shippingStepId}
+            >
+              Goods/Commodity
+            </ShippingStep>
+          </div>
+          <div className="lg:grid lg:grid-cols-4 flex flex-col gap-d-12 lg:mb-[34px] mb-[26px]">
+            <Check label="Add Extra Pickups" />
+            <Check label="Add Extra Drops" />
+            <div className="lg:block hidden" />
+            <Check label="Add More Goods/Commodities" />
+          </div>
         </div>
-        <div className="lg:grid lg:grid-cols-4 flex flex-col gap-d-12 lg:mb-[34px] mb-[26px]">
-          <Check label="Add Extra Pickups" />
-          <Check label="Add Extra Drops" />
-          <div className="lg:block hidden" />
-          <Check label="Add More Goods/Commodities" />
+        <div className="lg:pt-[32px]">
+          <Button
+            size="sm"
+            glossy
+            className="lg:w-auto w-full"
+            onClick={(e) => {
+              handleButtonClick(e, data)
+              setDisplayRate(true)
+            }}
+          >
+            Search
+          </Button>
         </div>
       </div>
-      <div className="lg:pt-[32px]">
-        <Button size="sm" glossy className="lg:w-auto w-full" onClick={(e) => handleButtonClick(e, data)}>
-          Search
-        </Button>
-      </div>
-    </div>
+
+      {isDisplayRate && (
+        <>
+          <GradientHR />
+          <div>Load Details</div>
+          <div>Details go here</div>
+        </>
+      )}
+    </>
   )
 }
 
