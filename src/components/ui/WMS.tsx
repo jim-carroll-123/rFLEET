f75883de250ff779e82bef1bfb8455d61a6e87a5
Luke Cutting <lukecutting123@gmail.com>
2023-11-08 09:51:53 -0600

    adding wms

---
+++ src/components/ui/WMS.tsx
@@ -0,0 +1,135 @@
+export const WMS = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <mask
+      id="mask0_500_51435"
+      //style="mask-type:luminance"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="20"
+      height="20"
+    >
+      <path d="M20 0H0V20H20V0Z" fill="white" />
+    </mask>
+    <g mask="url(#mask0_500_51435)">
+      <path
+        d="M15.3125 15V12.5H12.8125V15"
+        stroke="white"
+        stroke-opacity="0.5"
+        stroke-width="0.8"
+        stroke-miterlimit="10"
+      />
+      <path d="M14.0625 12.5V13.75" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path d="M15.3125 15V16.25" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path d="M12.8125 15V16.25" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path
+        d="M16.5625 17.5V15H11.5625V17.5"
+        stroke="white"
+        stroke-opacity="0.5"
+        stroke-width="0.8"
+        stroke-miterlimit="10"
+      />
+      <path d="M14.0625 15V17.5" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path
+        d="M5.3125 9.375H17.8125V17.5"
+        stroke="white"
+        stroke-opacity="0.5"
+        stroke-width="0.8"
+        stroke-miterlimit="10"
+      />
+      <path d="M10 5H10.625" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path d="M11.25 5H11.875" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <path d="M12.5 5H13.125" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <mask
+        id="mask1_500_51435"
+        // style="mask-type:luminance"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="20"
+        height="20"
+      >
+        <path d="M0 1.90735e-06H20V20H0V1.90735e-06Z" fill="white" />
+      </mask>
+      <g mask="url(#mask1_500_51435)">
+        <path d="M3.4375 6.25H19.6875" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      </g>
+      <path d="M7.8125 7.5H3.4375" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <mask
+        id="mask2_500_51435"
+        //style="mask-type:luminance"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="20"
+        height="20"
+      >
+        <path d="M0 1.90735e-06H20V20H0V1.90735e-06Z" fill="white" />
+      </mask>
+      <g mask="url(#mask2_500_51435)">
+        <path d="M19.6875 7.5H8.4375" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      </g>
+      <path
+        d="M5.3125 11.875H7.8125L10.3125 14.375V17.5"
+        stroke="white"
+        stroke-opacity="0.5"
+        stroke-width="0.8"
+        stroke-miterlimit="10"
+      />
+      <path d="M10.3125 14.375H5.3125" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+      <mask
+        id="mask3_500_51435"
+        // style="mask-type:luminance"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="20"
+        height="20"
+      >
+        <path d="M0 1.90735e-06H20V20H0V1.90735e-06Z" fill="white" />
+      </mask>
+      <g mask="url(#mask3_500_51435)">
+        <path d="M6.5625 17.5H0" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+        <path d="M0 8.75H5.3125V17.5" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+        <path d="M0 15H5.3125" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+        <path
+          d="M9.0625 17.5C9.0625 18.1904 8.50285 18.75 7.8125 18.75C7.12215 18.75 6.5625 18.1904 6.5625 17.5C6.5625 16.8096 7.12215 16.25 7.8125 16.25C8.50285 16.25 9.0625 16.8096 9.0625 17.5Z"
+          stroke="white"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+          stroke-miterlimit="10"
+        />
+        <path d="M7.5 17.5H8.125" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+        <path d="M1.25 16.25H4.375" stroke="white" stroke-opacity="0.5" stroke-width="0.8" stroke-miterlimit="10" />
+        <path
+          d="M5.9375 15.3125H7.1875"
+          stroke="white"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+          stroke-miterlimit="10"
+        />
+        <path
+          d="M19.6875 12.1875V17.5H9.0625"
+          stroke="white"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+          stroke-miterlimit="10"
+        />
+        <path
+          d="M3.4375 8.75V6.25L11.5625 1.25L19.6875 6.25V11.5625"
+          stroke="white"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+          stroke-miterlimit="10"
+        />
+        <path
+          d="M5.3125 10.625H17.8125"
+          stroke="white"
+          stroke-opacity="0.5"
+          stroke-width="0.8"
+          stroke-miterlimit="10"
+        />
+      </g>
+    </g>
+  </svg>
+)
