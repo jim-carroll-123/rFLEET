ff7757f671f2a25f05b3e9cafd1682c53b11e374
Luke Cutting <lukecutting123@gmail.com>
2023-11-14 21:39:47 -0600

    new quote updates

--- src/app/internal/newquote/page.tsx
+++ src/app/internal/newquote/page.tsx
@@ -5,7 +5,7 @@ export const NewQuoteSection = () => {
   return (
     <main>
       <div className="bg-[#141943] h-12"></div>
-      <div className="relative p-8 bg-[#F6F7FF] h-[100vh] w-[82vw] overflow-hidden">
+      <div className="relative p-8 bg-[#F6F7FF]">
         <Header title="New Quote" />
         <QuoteSection />
       </div>
--- src/app/internal/newquote/quotesection.tsx
+++ src/app/internal/newquote/quotesection.tsx
@@ -1,7 +1,111 @@
-import React from 'react'
+import React from 'react';
+
+
+
+import { Input } from '@components/ui/InputInternal'
 
 const QuoteSection = () => {
-  return <div className="text-black">Hello</div>
+  return (
+    <div className="grid grid-cols-2 gap-2 grid-rows-6 mt-5 text-black">
+      <div className="col-span-1 row-span-4 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl mb-4">Shipping From</h1>
+          <hr></hr>
+
+          <div className="grid grid-cols-2 gap-3 my-4">
+            <div className="col-span-2">
+              <div className="text-input font-semibold">Address Book</div>
+              <Input placeholder="Search Address Book" />
+            </div>
+            <div className="col-span-2">
+              <div className="text-input font-semibold ">Company Name</div>
+              <Input placeholder="Enter company name" />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Contact Name</div>
+              <Input placeholder="Enter contact name" />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Email</div>
+              <Input placeholder="Enter email" />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Phone Number</div>
+              <Input placeholder="Enter phone number" />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Phone Extension</div>
+              <Input placeholder="Enter extension" />
+            </div>
+            <div className="col-span-2">
+              <div className="text-input font-semibold">Address Line 1</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Address Line 2</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Address Line 3</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Country/Territory</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">Postal Code</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">State or Province</div>
+              <Input />
+            </div>
+            <div className="col-span-1">
+              <div className="text-input font-semibold">City</div>
+              <Input />
+            </div>
+          </div>
+          <hr></hr>
+        </div>
+      </div>
+
+      <div className="col-span-1 row-span-3 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl ">Service</h1>
+          <p className="text-sm ">This is the quote section</p>
+        </div>
+      </div>
+
+      <div className="col-span-1 row-span-1 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl ">Quote Section</h1>
+          <p className="text-sm ">This is the quote section</p>
+        </div>
+      </div>
+
+      <div className="col-span-1 row-span-2 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl ">Quote Section</h1>
+          <p className="text-sm ">This is the quote section</p>
+        </div>
+      </div>
+
+      <div className="col-span-1 row-span-1 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl ">Quote Section</h1>
+          <p className="text-sm ">This is the quote section</p>
+        </div>
+      </div>
+
+      <div className="col-span-1 row-span-1 bg-white rounded-lg border border-[#B8BEF8]">
+        <div className="flex flex-col p-4">
+          <h1 className="text-2xl ">Quote Section</h1>
+          <p className="text-sm ">This is the quote section</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default QuoteSection
