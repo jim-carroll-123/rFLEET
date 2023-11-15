9fdfd5c898ce53f22a180a6707d4e98de68b451c
Luke Cutting <lukecutting123@gmail.com>
2023-11-14 18:28:57 -0600

    updates

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -5,18 +5,21 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 
-import Link from 'next/link'
+import Link from 'next/link';
 
-import { WMSSection } from './sections/WMS/page'
-import { AddressBookSection } from './sections/address-book/page'
-import { DashboardSection } from './sections/dashboard/page'
-import { ECommerceSection } from './sections/ecommerce/page'
-import { NewShipmentSection } from './sections/new-shipment/page'
-import { NewPFQSection } from './sections/newPFQ/page'
-import { NewQuoteSection } from './sections/newquote/page'
-import { ProductSection } from './sections/product/page'
-import { ReportsSection } from './sections/reports/page'
-import { ShipmentProfileSection } from './sections/shipment-profile/page'
+
+
+import { WMSSection } from './sections/WMS/page';
+import { AddressBookSection } from './sections/address-book/page';
+import { DashboardSection } from './sections/dashboard/page';
+import { ECommerceSection } from './sections/ecommerce/page';
+import { NewShipmentSection } from './sections/new-shipment/page';
+import { NewPFQSection } from './sections/newPFQ/page';
+import { NewQuoteSection } from './sections/newquote/page';
+import { ProductSection } from './sections/product/page';
+import { ReportsSection } from './sections/reports/page';
+import { ShipmentProfileSection } from './sections/shipment-profile/page';
+
 
 // Define the props that Home component accepts
 interface HomeProps {
@@ -24,66 +27,9 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
-  const [currentHash, setCurrentHash] = useState<string>(() =>
-    typeof window !== 'undefined' ? window.location.hash : ''
-  )
-
-  useEffect(() => {
-    if (typeof window === 'undefined') {
-      return
-    }
-
-    const handleHashChange = () => {
-      setCurrentHash(window.location.hash)
-    }
-
-    window.addEventListener('hashchange', handleHashChange)
-
-    return () => {
-      window.removeEventListener('hashchange', handleHashChange)
-    }
-  }, [])
-
   return (
     <ParallaxProvider>
-      <PageContent currentHash={currentHash} />
+      <DashboardSection />
     </ParallaxProvider>
   )
-}
-
-// Define the props that PageContent component accepts
-interface PageContentProps {
-  currentHash: string
-}
-
-const PageContent = ({ currentHash }: PageContentProps) => {
-  const renderSection = () => {
-    switch (currentHash) {
-      case '#dashboard':
-        return <DashboardSection />
-      case '#new-quote':
-        return <NewQuoteSection />
-      case '#new-shipment':
-        return <NewShipmentSection />
-      case '#e-commerce':
-        return <ECommerceSection />
-      case '#product':
-        return <ProductSection />
-      case '#wms':
-        return <WMSSection />
-      case '#addressbook':
-        return <AddressBookSection />
-      case '#shipmentprofile':
-        return <ShipmentProfileSection />
-      case '#newpfq-newrfb':
-        return <NewPFQSection />
-      case '#reports':
-        return <ReportsSection />
-
-      default:
-        return <DashboardSection />
-    }
-  }
-
-  return <main>{renderSection()}</main>
 }
