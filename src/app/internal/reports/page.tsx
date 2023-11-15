a1ac410a02596b8bc6fac01bffb10548f7bc2f88
Luke Cutting <lukecutting123@gmail.com>
2023-11-15 09:44:57 -0600

    updates

--- src/app/internal/reports/page.tsx
+++ src/app/internal/reports/page.tsx
@@ -5,7 +5,7 @@ const ReportsSection = () => {
   return (
     <main>
       <div className="bg-[#141943] h-12"></div>
-      <div className="relative p-8 bg-[#F6F7FF] h-[100vh] w-[80vw] overflow-hidden">
+      <div className="relative p-8 bg-[#F6F7FF] h-[100vh]">
         <Header title="Reports" />
       </div>
     </main>
